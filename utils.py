f5755c0f4b6bae37482a7ee00fa24753fda06dea
paulairazoqui <paulairazoqui@gmail.com>
2024-12-13 09:04:57 -0300

    corrigiendo indentacion

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ def cantidad_filmaciones_mes(Mes):
     movies = pd.read_csv(movies_path)
         
     # Diccionario para convertir el mes en español al número correspondiente
-        meses = {
+    meses = {
         "enero": 1, "febrero": 2, "marzo": 3, "abril": 4,
         "mayo": 5, "junio": 6, "julio": 7, "agosto": 8,
         "septiembre": 9, "octubre": 10, "noviembre": 11, "diciembre": 12
